65fbade44056fed9e8192abd3ee1e3e8e18bdabf
acdmAnderson <acdm.anderson@hotmail.com>
2020-12-23 23:37:37 -0300

    feat: ensure SignUpResolver return an 400 if lastName it's not provided

--- nest+graphql+angular/server/src/presentation/controllers/signup.ts
+++ nest+graphql+angular/server/src/presentation/controllers/signup.ts
@@ -1,8 +1,16 @@
 export class SingUpResolver {
   handle (httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Field fistName shold be prodided')
+    if (!httpRequest.body.fistName) {
+      return {
+        statusCode: 400,
+        body: new Error('Field fistName shold be prodided')
+      }
+    }
+    if (!httpRequest.body.lastName) {
+      return {
+        statusCode: 400,
+        body: new Error('Field lastName shold be prodided')
+      }
     }
   }
 }
