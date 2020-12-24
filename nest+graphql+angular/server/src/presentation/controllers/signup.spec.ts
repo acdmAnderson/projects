8ea5098c15b3300f62cdc0116c38f1160cadb288
acdmAnderson <acdm.anderson@hotmail.com>
2020-12-23 23:38:15 -0300

    test: ensure SignUpResolver return an 400 if lastName it's not provided

--- nest+graphql+angular/server/src/presentation/controllers/signup.spec.ts
+++ nest+graphql+angular/server/src/presentation/controllers/signup.spec.ts
@@ -15,4 +15,19 @@ describe('Sign Up Resolver', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Field fistName shold be prodided'))
   })
+
+  test('Shold return an 400 if no lastName is provided', () => {
+    const rut = new SingUpResolver()
+    const httpRequest = {
+      body: {
+        fistName: 'any_fistName',
+        email: 'any_email',
+        password: 'any_password',
+        isActive: true
+      }
+    }
+    const httpResponse = rut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Field lastName shold be prodided'))
+  })
 })
