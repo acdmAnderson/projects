09619547fc5c6d7d40a9ca1f1693b855598a9924
acdmAnderson <acdm.anderson@hotmail.com>
2020-12-27 22:33:07 -0300

    feat: ensure SignUpController return 400 if no email is provided

--- nest+graphql+angular/server/src/presentation/controllers/signup.ts
+++ nest+graphql+angular/server/src/presentation/controllers/signup.ts
@@ -3,7 +3,7 @@ import { MissingFieldError } from '../error/missing-field.error'
 import { badRequest } from '../helpers/http.helper'
 
 export class SingUpResolver {
-  private readonly requiredFields = ['fistName', 'lastName']
+  private readonly requiredFields = ['fistName', 'lastName', 'email']
   handle (httpRequest: HttpRequest): HttpResponse {
     for (const field of this.requiredFields) {
       if (!httpRequest.body[field]) return badRequest(new MissingFieldError(field))
