cec411f1d3a82b9193d18692f995e1d766eb6861
acdmAnderson <acdm.anderson@hotmail.com>
2020-12-27 22:43:57 -0300

    refactor: make SignUpController conforms to a contracts

--- nest+graphql+angular/server/src/presentation/controllers/signup.ts
+++ nest+graphql+angular/server/src/presentation/controllers/signup.ts
@@ -1,8 +1,9 @@
+import { Controller } from '../contracts/controller'
 import { HttpRequest, HttpResponse } from '../contracts/http'
 import { MissingFieldError } from '../error/missing-field.error'
 import { badRequest } from '../helpers/http.helper'
 
-export class SingUpResolver {
+export class SingUpResolver implements Controller {
   private readonly requiredFields = ['fistName', 'lastName', 'email', 'password']
   handle (httpRequest: HttpRequest): HttpResponse {
     for (const field of this.requiredFields) {
