d3bc8e45dbdababab4edeeb1e06c1b5b482c532c
acdmAnderson <acdm.anderson@hotmail.com>
2021-01-06 22:57:03 -0300

    feat: ensure EmailValidatorAdapter returns true if validator returns true

--- nest+graphql+angular/server/src/utils/email-validator-adapter.ts
+++ nest+graphql+angular/server/src/utils/email-validator-adapter.ts
@@ -1,7 +1,7 @@
 import { EmailValidator } from '../presentation/contracts/email-validator'
-
+import validator from 'validator'
 export class EmailValidatorAdapter implements EmailValidator {
   isValid (email: string): boolean {
-    return false
+    return validator.isEmail(email)
   }
 }
