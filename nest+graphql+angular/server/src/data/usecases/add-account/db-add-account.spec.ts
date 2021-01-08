3120fb5441c7259eb48fb8b37a52d9bb2ef476cf
acdmAnderson <acdm.anderson@hotmail.com>
2021-01-07 22:57:27 -0300

    refactor: move sut creation to a factory helper method

--- nest+graphql+angular/server/src/data/usecases/add-account/db-add-account.spec.ts
+++ nest+graphql+angular/server/src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,14 +1,26 @@
+import { Encrypter } from 'src/data/contracts/encrypter'
 import { DbAddAccount } from './db-add-account'
 
+interface SutTypes {
+  sut: DbAddAccount
+  encrypterStub: Encrypter
+}
+const makeSut = (): SutTypes => {
+  class EncrypterStub implements Encrypter {
+    async encrypt (value: string): Promise<string> {
+      return new Promise(resolve => resolve('hashed_password'))
+    }
+  }
+  const encrypterStub = new EncrypterStub()
+  const sut = new DbAddAccount(encrypterStub)
+  return {
+    sut,
+    encrypterStub
+  }
+}
 describe('DbAddAccount Usecase', () => {
   test('Should call Encrypter with correct password', async () => {
-    class EncrypterStub {
-      async encrypt (_value: string): Promise<string> {
-        return new Promise(resolve => resolve('hashed_password'))
-      }
-    }
-    const encrypterStub = new EncrypterStub()
-    const sut = new DbAddAccount(encrypterStub)
+    const { sut, encrypterStub } = makeSut()
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
     const accountData = {
       firstName: 'valid_firstName',
