6a1fde41a45f18585be3a1d16ee6d8fcfb5d1c4e
acdmAnderson <acdm.anderson@hotmail.com>
2021-01-08 23:40:43 -0300

    refactor: move sut creation to a factory method

--- nest+graphql+angular/server/src/infra/criptography/bcrypt-adapter.spec.ts
+++ nest+graphql+angular/server/src/infra/criptography/bcrypt-adapter.spec.ts
@@ -8,22 +8,25 @@ jest.mock('bcrypt', () => ({
 }))
 describe('Bcrypt Adapter', () => {
   const salt = 12
+  const makeSut = (): BcryptAdapter => {
+    return new BcryptAdapter(salt)
+  }
   test('Should calls encrypt with correct value', async () => {
-    const sut = new BcryptAdapter(salt)
+    const sut = makeSut()
     const encryptSpy = jest.spyOn(sut, 'encrypt')
     await sut.encrypt('any_value')
     expect(encryptSpy).toHaveBeenCalledWith('any_value')
   })
 
   test('Should calls bcrypt with correct values', async () => {
-    const sut = new BcryptAdapter(salt)
+    const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
   test('Should return hash on success', async () => {
-    const sut = new BcryptAdapter(salt)
+    const sut = makeSut()
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hash')
   })
