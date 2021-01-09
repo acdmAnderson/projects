1b3f0969f2900058bb4eb3f3fa36b95a3c808acf
acdmAnderson <acdm.anderson@hotmail.com>
2021-01-08 23:29:02 -0300

    test: ensure BcryptAdapter call bcrypt with correct values

--- nest+graphql+angular/server/src/infra/criptography/bcrypt-adapter.spec.ts
+++ nest+graphql+angular/server/src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,10 +1,19 @@
 import { BcryptAdapter } from './bcrypt-adapter'
+import bcrypt from 'bcrypt'
 
 describe('Bcrypt Adapter', () => {
+  const salt = 12
   test('Should calls encrypt with correct value', async () => {
-    const sut = new BcryptAdapter()
+    const sut = new BcryptAdapter(salt)
     const encryptSpy = jest.spyOn(sut, 'encrypt')
     await sut.encrypt('any_value')
     expect(encryptSpy).toHaveBeenCalledWith('any_value')
+  })
+
+  test('Should calls bcrypt with correct values', async () => {
+    const sut = new BcryptAdapter(salt)
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+    await sut.encrypt('any_value')
+    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 })
