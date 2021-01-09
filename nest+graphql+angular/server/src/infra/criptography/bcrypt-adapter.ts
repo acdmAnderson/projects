9a721582a0c20bfbf8f11b4aedc7b82cb76aeeb3
acdmAnderson <acdm.anderson@hotmail.com>
2021-01-08 23:28:28 -0300

    feat: ensure BcryptAdapter call bcrypt with correct values

--- nest+graphql+angular/server/src/infra/criptography/bcrypt-adapter.ts
+++ nest+graphql+angular/server/src/infra/criptography/bcrypt-adapter.ts
@@ -1,7 +1,14 @@
 import { Encrypter } from '../../data/contracts/encrypter'
+import bcrypt from 'bcrypt'
 
 export class BcryptAdapter implements Encrypter {
+  private readonly salt: number
+  constructor (salt: number) {
+    this.salt = salt
+  }
+
   async encrypt (value: string): Promise<string> {
-    return new Promise(resolve => resolve(null))
+    await bcrypt.hash(value, this.salt)
+    return null
   }
 }
