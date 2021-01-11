bbc326ee1ffbcc42aceddf55acb07a365dfffaac
acdmAnderson <acdm.anderson@hotmail.com>
2021-01-11 20:44:26 -0300

    test: ensure AccountMongoRepository return an account on success

---
+++ nest+graphql+angular/server/src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,27 @@
+import { MongoHelper } from '../helpers/mongo.helper'
+import { AccountMongoRepository } from './account'
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      firstName: 'any_firstName',
+      lastName: 'any_lastName',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.firstName).toBe('any_firstName')
+    expect(account.lastName).toBe('any_lastName')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
+  })
+})
