52bb505f72f048561bdec397a3709a14a4cdd08f
acdmAnderson <acdm.anderson@hotmail.com>
2021-01-11 20:58:45 -0300

    chore: add ignore watch on mongo config

--- nest+graphql+angular/server/jest.config.js
+++ nest+graphql+angular/server/jest.config.js
@@ -10,5 +10,6 @@ module.exports = {
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
-  preset: '@shelf/jest-mongodb'
+  preset: '@shelf/jest-mongodb',
+  watchPathIgnorePatterns: ['globalConfig']
 }
