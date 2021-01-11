fd0d4ca030e5601d4974221a66fe8c41425badcb
acdmAnderson <acdm.anderson@hotmail.com>
2021-01-11 20:42:38 -0300

    feat: ensure AccountMongoRepository return an account on success

--- nest+graphql+angular/server/src/infra/db/mongodb/helpers/mongo.helper.ts
+++ nest+graphql+angular/server/src/infra/db/mongodb/helpers/mongo.helper.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 export const MongoHelper = {
   client: null as MongoClient,
   async connect (uri: string): Promise<void> {
@@ -10,5 +10,9 @@ export const MongoHelper = {
   },
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
